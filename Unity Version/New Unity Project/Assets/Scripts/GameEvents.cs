82a3b4ec67c0a897d2909885cc7d95b970612fad
Barend Noordhoff <thedjshinx@gmail.com>
2023-11-14 22:20:49 +0100

    Basic unstyled ui made

---
+++ Unity Version/New Unity Project/Assets/Scripts/GameEvents.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameEvents : MonoBehaviour
+{
+    [SerializeField] 
+    private int round;
+
+    private void Start()
+    {
+        StartLoadInAnimation();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        GameObject roundText = GameObject.FindGameObjectWithTag("RoundText");
+        roundText.GetComponent<Text>().text = string.Format(""+round+"");
+    }
+
+    
+    /**
+     * Methods.
+     */
+    void RoundIsOver()
+    {
+        round++;
+        StartRoundSwitchAnimation();
+    }
+
+    void StartLoadInAnimation()
+    {
+        
+    }
+    
+    void StartRoundSwitchAnimation()
+    {
+        
+    }
+}
--- Unity Version/New Unity Project/Assets/Scripts/determinePosition.cs
+++ Unity Version/New Unity Project/Assets/Scripts/determinePosition.cs
@@ -22,34 +22,15 @@ public class DeterminePosition : MonoBehaviour
     void Update()
     {
         float pos = (player1.transform.position.x + player2.transform.position.x) / 2;
-        //float diff = player2.transform.position.x - player1.transform.position.x;
         
         LookAtSubject(pos);
-        //BackOffAccordingly(diff);
     }
 
     void LookAtSubject(float pos)
     {
         Vector3 newPos = _cameraTransform.position;
         newPos.x = pos;
-        
-        _cameraTransform.position = newPos;  
-    }
 
-    // ReSharper disable Unity.PerformanceAnalysis
-    /*void BackOffAccordingly(float diff)
-    {
-        var position = _camera.transform.position;
-        
-        if (diff > 13)
-        {
-            Debug.Log("blah");
-            Mathf.Lerp(position.z, position.z + 102222, 2f);
-        }
-        else
-        {
-            Debug.Log("no blah");
-            Mathf.Lerp(position.z + 102222, position.z , 2f);
-        }
-    }*/
+        _cameraTransform.position = Vector3.Lerp(_cameraTransform.position, newPos, 10f * Time.deltaTime);
+    }
 }
