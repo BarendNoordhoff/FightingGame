bc52845c9bbb976b90ab4111dabb15a7aced9c20
Barend Noordhoff <thedjshinx@gmail.com>
2023-11-14 21:56:53 +0100

    ALOT of srs stuff which is good, and some camera stuff
    To see what the srs changes are check the version controll

--- Unity Version/New Unity Project/Assets/Scripts/determinePosition.cs
+++ Unity Version/New Unity Project/Assets/Scripts/determinePosition.cs
@@ -1,18 +1,55 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using System;
 using UnityEngine;
 
-public class determinePosition : MonoBehaviour
+public class DeterminePosition : MonoBehaviour
 {
     [SerializeField] 
     private GameObject player1;
     [SerializeField] 
     private GameObject player2;  
     // Update is called once per frame
+
+    private Transform _cameraTransform;
+
+    [SerializeField] 
+    private Camera _camera;
+    
+    private void Start()
+    {
+        _cameraTransform = this.transform;
+    }
+
     void Update()
     {
-        Vector3 newPos = this.transform.position;
-        newPos.x = (player1.transform.position.x + player2.transform.position.x) / 2;
-        this.transform.position = newPos;
+        float pos = (player1.transform.position.x + player2.transform.position.x) / 2;
+        //float diff = player2.transform.position.x - player1.transform.position.x;
+        
+        LookAtSubject(pos);
+        //BackOffAccordingly(diff);
     }
+
+    void LookAtSubject(float pos)
+    {
+        Vector3 newPos = _cameraTransform.position;
+        newPos.x = pos;
+        
+        _cameraTransform.position = newPos;  
+    }
+
+    // ReSharper disable Unity.PerformanceAnalysis
+    /*void BackOffAccordingly(float diff)
+    {
+        var position = _camera.transform.position;
+        
+        if (diff > 13)
+        {
+            Debug.Log("blah");
+            Mathf.Lerp(position.z, position.z + 102222, 2f);
+        }
+        else
+        {
+            Debug.Log("no blah");
+            Mathf.Lerp(position.z + 102222, position.z , 2f);
+        }
+    }*/
 }
